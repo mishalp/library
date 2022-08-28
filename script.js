021c0511046c9f37b9befbcd77e09cf5125a51b4
mishalp <mishalclever@gmail.com>
2022-08-28 17:03:52 +0530

    Model created

---
+++ script.js
@@ -0,0 +1,78 @@
+const container = document.getElementById('list');
+
+let myLibrary = [];
+
+displayBook();
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBook() {
+   var title = document.getElementById('title').value;
+   var author = document.getElementById('author').value;
+   var pages = document.getElementById('pages').value;
+    var isRead = "readed";
+    const book = new Book(title, author, pages, isRead);
+    myLibrary.push(book);
+    
+    displayBook()
+}
+
+function displayBook() {
+    
+    container.innerHTML =""
+    myLibrary.forEach((book, data) => {
+        let div = document.createElement('div');
+        var remove = document.createElement('button');
+        var read = document.createElement('button')
+        read.classList.add('read')
+        remove.classList.add('remove')
+        read.innerHTML = book.read
+        remove.innerHTML = "remove";
+        div.classList.add('card');
+         book.index = data;
+         remove.setAttribute('data-index', `${book.index}`)
+         read.setAttribute('data-index', `${book.index}`)
+        div.innerHTML = `<h2>${book.title}</h2> by <h3>${book.author}</h3>`
+        div.appendChild(remove)
+        div.appendChild(read)
+        container.appendChild(div);
+    });
+    btnListen()
+    
+}
+
+function btnListen(){
+    const btnRemove = document.querySelectorAll('.remove');
+    btnRemove.forEach(btn =>{
+    btn.addEventListener('click', (e)=>{
+        removebtn(e.path[0].dataset.index);
+    })
+   })
+
+   const readbtns = document.querySelectorAll('.read');
+   readbtns.forEach(btn =>{
+    btn.addEventListener('click', (e)=>{
+        toggle(e.path[0].dataset.index, btn.innerHTML)
+    })
+   })
+}
+
+function toggle(index, read){
+    if(read == 'readed'){
+        myLibrary[index].read = "Not readed"
+    }else{
+        myLibrary[index].read = "readed"
+    }
+    displayBook()
+    
+}
+
+function removebtn(data){
+    myLibrary.splice(data,1);
+     displayBook()
+}
