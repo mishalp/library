3be610d23c5ad33869a81127409f364191c3564e
mishalp <mishalclever@gmail.com>
2022-09-02 17:53:19 +0530

    classified

--- script.js
+++ script.js
@@ -1,88 +1,98 @@
-const container = document.getElementById('list');
 
-let myLibrary = [];
-
-displayBook();
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBook() {
-   var title = document.getElementById('title').value;
-   var author = document.getElementById('author').value;
-   var pages = document.getElementById('pages').value;
-    var isRead = document.getElementById('readit').value
-    if(title == '' || author == '' || pages == ''){
-        alert("fields Cannot be empty!!")
-        return;
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-    const book = new Book(title, author, pages, isRead);
-    myLibrary.push(book);
-    
-    document.getElementById('title').value = ""
-    document.getElementById('author').value = ""
-    document.getElementById('pages').value = ""
-
-    displayBook()
 }
 
-function displayBook() {
-    
-    container.innerHTML =""
-    myLibrary.forEach((book, data) => {
-        let div = document.createElement('div');
-        var remove = document.createElement('button')
-        var btnDiv = document.createElement('div');
-        var read = document.createElement('button')
-        read.classList.add('read')
-        remove.classList.add('remove')
-        read.innerHTML = book.read
-        remove.innerHTML = "remove";
-        div.classList.add('card');
-         book.index = data;
-         remove.setAttribute('data-index', `${book.index}`)
-         read.setAttribute('data-index', `${book.index}`)
-        div.innerHTML = `<h2>${book.title}</h2> by <h3>${book.author}</h3><p>${book.pages} Pages`
-        btnDiv.appendChild(remove)
-        btnDiv.appendChild(read)
-        div.appendChild(btnDiv)
-        container.appendChild(div);
-    });
-    btnListen()
-    
-}
 
-function btnListen(){
-    const btnRemove = document.querySelectorAll('.remove');
-    btnRemove.forEach(btn =>{
-    btn.addEventListener('click', (e)=>{
-        removebtn(e.path[0].dataset.index);
-    })
-   })
+class Library{
+    constructor(){
+        this.container = document.getElementById('list');
+        this.myLibrary = [];
 
-   const readbtns = document.querySelectorAll('.read');
-   readbtns.forEach(btn =>{
-    btn.addEventListener('click', (e)=>{
-        toggle(e.path[0].dataset.index, btn.innerHTML)
-    })
-   })
-}
-
-function toggle(index, read){
-    if(read == 'readed'){
-        myLibrary[index].read = "Not readed"
-    }else{
-        myLibrary[index].read = "readed"
+        this.displayBook();
     }
-    displayBook()
+
     
+    addBook() {
+       var title = document.getElementById('title').value;
+       var author = document.getElementById('author').value;
+       var pages = document.getElementById('pages').value;
+        var isRead = document.getElementById('readit').value
+        if(title == '' || author == '' || pages == ''){
+            alert("fields Cannot be empty!!")
+            return;
+        }
+        const book = new Book(title, author, pages, isRead);
+        this.myLibrary.push(book);
+        
+        document.getElementById('title').value = ""
+        document.getElementById('author').value = ""
+        document.getElementById('pages').value = ""
+    
+        this.displayBook()
+    }
+    
+    displayBook() {
+        
+        this.container.innerHTML =""
+        this.myLibrary.forEach((book, data) => {
+            let div = document.createElement('div');
+            var remove = document.createElement('button')
+            var btnDiv = document.createElement('div');
+            var read = document.createElement('button')
+            read.classList.add('read')
+            remove.classList.add('remove')
+            read.innerHTML = book.read
+            remove.innerHTML = "remove";
+            div.classList.add('card');
+             book.index = data;
+             remove.setAttribute('data-index', `${book.index}`)
+             read.setAttribute('data-index', `${book.index}`)
+            div.innerHTML = `<h2>${book.title}</h2> by <h3>${book.author}</h3><p>${book.pages} Pages`
+            btnDiv.appendChild(remove)
+            btnDiv.appendChild(read)
+            div.appendChild(btnDiv)
+            this.container.appendChild(div);
+        });
+        this.btnListen()
+        
+    }
+    
+    btnListen(){
+        const btnRemove = document.querySelectorAll('.remove');
+        btnRemove.forEach(btn =>{
+        btn.addEventListener('click', (e)=>{
+            this.removebtn(e.composedPath()[0].dataset.index);
+        })
+       })
+    
+       const readbtns = document.querySelectorAll('.read');
+       readbtns.forEach(btn =>{
+        btn.addEventListener('click', (e)=>{
+            this.toggle(e.composedPath()[0].dataset.index, btn.innerHTML)
+        })
+       })
+    }
+    
+    toggle(index, read){
+        if(read == 'readed'){
+            this.myLibrary[index].read = "Not readed"
+        }else{
+            this.myLibrary[index].read = "readed"
+        }
+        this.displayBook()
+        
+    }
+    
+    removebtn(data){
+        this.myLibrary.splice(data,1);
+        this.displayBook()
+    }
 }
 
-function removebtn(data){
-    myLibrary.splice(data,1);
-     displayBook()
-}
+let library = new Library()
